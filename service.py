01e1786834a7fa268d8ecd7f33597d44cd5a3e81
Hasan Kemal Türk <kemalhasan03.03@gmail.com>
2022-03-18 11:50:28 +0300

    DB CRUD Operations Done

--- service.py
+++ service.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-
 models.Base.metadata.create_all(engine)
 
 def get_db():
@@ -15,7 +14,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
 
 @app.get("/get-all",status_code=status.HTTP_200_OK)
 def getStudents(db: Session = Depends(get_db)):
@@ -30,24 +28,29 @@ def getById(id:int,response:Response,db: Session = Depends(get_db)):
         return HTTPException(status_code=404,detail=f"No student found with id {id}")
     return student
 
-
 @app.post("/create-student",status_code=status.HTTP_200_OK)
 def createStudent(request: schemas.Student,db:Session = Depends(get_db)):
     new_student = models.Student(firstName=request.firstName,lastName=request.lastName,number= request.number)
     db.add(new_student)
     db.commit()
     db.refresh(new_student)
-
     return new_student
 
 @app.put("/update-student/{id}",status_code=status.HTTP_200_OK)
-async def updateStudent(id:int,request: schemas.Student,db: Session = Depends(get_db)):
-    await db.query(models.Student).filter(models.Student.id == id).update(schemas.Student(firstName=request.firstName,lastName=request.lastName,number=request.number))
-    # if not student.first():
-    #     return HTTPException(status_code=404,detail=f"No student found with id {id}")
-    # student.update(request)
+def updateStudent(id:int,request: schemas.UpdateStudent,db: Session = Depends(get_db)):
+    student = db.query(models.Student).filter(models.Student.id == id)
+
+    if not student:
+        return HTTPException(status_code=404,detail=f"No student found with id {id}")
+    
+    student.update({
+        "firstName": request.firstName,
+        "lastName": request.lastName,
+        "number": request.number, 
+    })
     db.commit()
-    return "Student info updated"
+    
+    return student.first()
 
 
 @app.delete("/delete-student/{id}",status_code=status.HTTP_200_OK)
